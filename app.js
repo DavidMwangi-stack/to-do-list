ac8cea760d0f2fc11474bb20be2bde0de16223af
DavidMwangi-stack <edwarddavidmwangi11@gmail.com>
2025-01-18 13:36:24 +0300

    second  commit

--- app.js
+++ app.js
@@ -16,6 +16,21 @@ document.querySelector('#push').onclick = function(){
         </div>`;
 
         var current_tasks = document.querySelectorAll(".delete");
-        
+        for (var i=0; i < current_tasks.length; i ++){
+          current_tasks[i].onclick = function(){
+            this.parentNode.remove();
+          }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+          tasks[i].onclick = function(){
+            this.classList.toggle('completed');
+          }
+        }
+
+        document.querySelector("#newtask input").
+        value = "";
+
     }
 }
